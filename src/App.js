1db070dff66ab13a927caf856bbcea28c3dfec2b
Sourabh <sourabhshakya1998@gmail.com>
2022-04-03 01:08:51 +0530

    update sign-in component and custom-button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route , Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage.component';
 import shopPage from './pages/shopPage/shop.component';
+import SignInAndSignUpPage from './pages/Sign-in-and-sign-up/Sign-in-and-sign-up.component';
 import Header from './components/header/Header.component';
 
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={shopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
        </Switch>
     </div> 
   );
